c259f93726594e024cffd87de34ad641785d81be
Daiki Nakashita <dnakashita@ovice.co>
2024-05-11 18:42:12 +0900

    move the user entity to userrepo

--- repository/userrepo/userrepo.go
+++ repository/userrepo/userrepo.go
@@ -3,10 +3,11 @@ package userrepo
 import (
 	"errors"
 	"fmt"
-	"graphyy/database"
 	"graphyy/entity"
 	"graphyy/internal/envvar"
+	"time"
 
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
@@ -25,7 +26,7 @@ func NewUserRepo(db *gorm.DB) *UserRepo {
 
 // GetExistingUser fetches a user by the username from the db and returns it.
 func (repo *UserRepo) GetExistingUser(username string) (*entity.User, error) {
-	var user *database.User
+	var user UserEntity
 	result := repo.db.Where("username = ? AND deleted_at IS NULL", username).First(&user)
 	if result.Error != nil {
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
@@ -48,7 +49,7 @@ func (repo *UserRepo) CreateUser(user entity.User) (*entity.User, error) {
 	if err != nil {
 		return nil, err
 	}
-	dbUser := database.User{
+	dbUser := UserEntity{
 		Username: user.Username,
 		Password: hashedPass,
 	}
@@ -71,4 +72,27 @@ func HashPassword(password string) (string, error) {
 	cost := envvar.HashCost()
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), cost)
 	return string(bytes), err
+}
+
+// use the entity suffix for the entities used for database operations
+// UserEntity struct represents the user entity in the db. entities defined in this package should only be used in the repository package.
+type UserEntity struct {
+	ID        uuid.UUID `gorm:"type:uuid;primary_key;"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Username  string         `gorm:"unique;index;not null"`
+	Password  string         `gorm:"type:varchar(1000);not null"`
+}
+
+// BeforeCreate will set a UUID rather than numeric ID.
+func (user *UserEntity) BeforeCreate(tx *gorm.DB) (err error) {
+	user.ID = uuid.New()
+	return
+}
+
+// TableName overrides the table name settings in Gorm to force a specific table name
+// in the database.
+func (user *UserEntity) TableName() string {
+	return "users"
 }
