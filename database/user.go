a759b2a9916e3f08186d5e0428f0b0e2b4413f80
Daiki Nakashita <dnakashita@ovice.co>
2024-05-11 13:20:39 +0900

    add a comment about db entities

--- database/user.go
+++ database/user.go
@@ -7,6 +7,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// User struct represents the user entity in the db. entities defined in this package should only be used in the database and repository packages.
 type User struct {
 	ID        uuid.UUID `gorm:"type:uuid;primary_key;"`
 	CreatedAt time.Time
